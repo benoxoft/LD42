424314f6efcdd16c4f36df56f6bd49afd34ed2ce
Benoit Paquet <benoxoft@gmail.com>
2018-08-08 21:13:45 -0400

    Initial commit

---
+++ desktop/src/com/ld42/ben/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ld42.ben.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ld42.ben.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
